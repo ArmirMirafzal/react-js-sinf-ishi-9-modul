fe4dae61e5176556da83a0b79b9988466919876e
Armir <mirarmirkhan@gmail.com>
2023-07-12 16:52:35 +0500

    next commit

--- src/services/http.ts
+++ src/services/http.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-const baseURL = "http://10.10.2.223:4000/api";
+const baseURL = "https://pdp-movies-78.onrender.com/api";
 
 export const http = axios.create({ baseURL });
