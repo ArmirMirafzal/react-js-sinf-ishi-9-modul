2628584e2f2d23d2e54d7e3972360fa21eadc83f
Armir <mirarmirkhan@gmail.com>
2023-07-17 15:43:38 +0500

    100-dars-sinf-ishi

--- src/components/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as Navbar } from "./navbar";
-export { default as Loader } from "./loader";
-export { default as Input } from "./input";
--- src/pages/index.ts
+++
@@ -1,8 +0,0 @@
-export { default as Home } from "./home/home";
-export { default as Login } from "./login";
-export { default as NewMovie } from "./new-movie";
-export { default as Register } from "./register";
-export { default as NewGenre } from "./new-genre";
-export { default as EditMovie } from "./edit-movie";
-
-
